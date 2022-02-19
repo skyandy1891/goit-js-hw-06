423082d994b89a7d3f35b37ec9c90e680a06a7db
skyandy1891 <skyandy1891@gmail.com>
2022-02-19 14:45:58 +0200

    add task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -13,3 +13,14 @@
 //     .padStart(6, 0)}`;
 // }
 
+const changeColorBtn = document.querySelector('.change-color');
+const outputSpan = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  outputSpan.textContent = document.body.style.backgroundColor;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
