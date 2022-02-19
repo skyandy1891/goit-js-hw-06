076c606f0f688881cd0eb47a4d0fa6cb97125977
skyandy1891 <skyandy1891@gmail.com>
2022-02-19 14:20:15 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -17,3 +17,28 @@
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+const refs = {
+    form: document.querySelector('.login-form'),
+}
+
+function formSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    console.log(formElements)
+    const mail = formElements.email.value;
+
+    const password = formElements.password.value;
+    
+    if (mail === '' || password === '') { 
+        alert('Все поля должны быть заполнены!')
+    }
+    
+    const formData = {
+        mail,
+        password,
+    }
+    console.log(formData)
+    refs.form.reset();
+}
+refs.form.addEventListener('submit', formSubmit);
